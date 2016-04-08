76f3b8ee4548de934d4f00cedd238561dcaaf5b5
Vince <vincent.loiseleur@gmail.com>
2016-04-08 14:01:21 -0700

    Query Particle devices registered to user after log-in.

--- app/src/main/java/com/sodomakerspace/particletron/DashboardActivity.java
+++ app/src/main/java/com/sodomakerspace/particletron/DashboardActivity.java
@@ -2,12 +2,69 @@ package com.sodomakerspace.particletron;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.util.List;
+
+import io.particle.android.sdk.cloud.ParticleCloud;
+import io.particle.android.sdk.cloud.ParticleCloudException;
+import io.particle.android.sdk.cloud.ParticleCloudSDK;
+import io.particle.android.sdk.cloud.ParticleDevice;
+import io.particle.android.sdk.utils.Async;
 
 public class DashboardActivity extends AppCompatActivity {
+
+    public TextView output;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        // Get our UI elements
+        output = (TextView) findViewById(R.id.output_textiView);
+
+        // Print the list of registered Particle devices for this account to our output log
+        getDevices();
+    }
+
+    private void getDevices () {
+
+        // Attempt to get the list of devices that belong to the currently logged-in user
+        Async.executeAsync(ParticleCloudSDK.getCloud(), new Async.ApiWork<ParticleCloud, Object>() {
+            @Override
+            public Object callApi(ParticleCloud particleCloud) throws ParticleCloudException, IOException {
+                List<ParticleDevice> devices = ParticleCloudSDK.getCloud().getDevices();
+                for (int i = 0; i < devices.size(); i++) {
+                    writeLine(devices.get(i).getName());
+                    writeLine(devices.get(i).getID());
+                }
+                return -1;
+            }
+
+            @Override
+            public void onSuccess(Object o) {
+
+            }
+
+            @Override
+            public void onFailure(ParticleCloudException exception) {
+                exception.printStackTrace();
+            }
+        });
+    }
+
+    // Write some text to the output text view.
+    // Care is taken to do this on the main UI thread so writeLine can be called
+    // from any thread (like the BTLE callback).
+    private void writeLine(final CharSequence text) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                output.append(text);
+                output.append("\n");
+            }
+        });
     }
 }
