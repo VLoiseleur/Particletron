b50bc0dd647ff370d494989294a3a3b319bb759b
Vince <vincent.loiseleur@gmail.com>
2016-05-16 00:54:24 -0700

    Added basic dashboard functionality - ability to create and move simple function buttons.

--- app/src/main/java/com/sodomakerspace/particletron/DashboardActivity.java
+++ app/src/main/java/com/sodomakerspace/particletron/DashboardActivity.java
@@ -1,28 +1,25 @@
 package com.sodomakerspace.particletron;
 
+import android.content.ClipData;
+import android.content.ClipDescription;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.KeyEvent;
+import android.view.DragEvent;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.Spinner;
-import android.widget.TextView;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 import io.particle.android.sdk.cloud.ParticleCloud;
 import io.particle.android.sdk.cloud.ParticleCloudException;
@@ -34,6 +31,11 @@ import io.particle.android.sdk.utils.Toaster;
 public class DashboardActivity extends AppCompatActivity {
 
     // UI elements
+    private RelativeLayout _dashboardLayout;
+
+    private boolean _editMode;
+
+    private List<ImageButton> _widgetList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +43,10 @@ public class DashboardActivity extends AppCompatActivity {
         setContentView(R.layout.activity_dashboard);
 
         // Get our UI elements
+        _dashboardLayout = (RelativeLayout) findViewById(R.id.dashboard_layout);
+
+        // Set mode type
+        _editMode = false;
 
         // Create our toolbar
         Toolbar dashboardToolbar = (Toolbar) findViewById(R.id.dashboard_toolbar);
@@ -49,6 +55,8 @@ public class DashboardActivity extends AppCompatActivity {
             dashboardToolbar.setTitle(R.string.appbarTitle_dashboard);
             setSupportActionBar(dashboardToolbar);
         }
+
+        // Set up the user's widget grid
     }
 
     @Override
@@ -61,6 +69,26 @@ public class DashboardActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
+            case R.id.action_edit:
+                // Enable drag and drop for widgets
+                if (_editMode) {
+                    item.setIcon(R.drawable.ic_create_black_48dp);
+                    disableEditMode();
+                    _editMode = false;
+                }
+                else {
+                    item.setIcon(R.drawable.ic_create_white_48dp);
+                    toggleEditMode();
+                    _editMode = true;
+                }
+                return true;
+
+            case R.id.action_new:
+                // Create a new widget on the dashboard
+                if (!_editMode)
+                    createNewWidget();
+                return true;
+
             case R.id.action_controller:
                 // Launch the controller, where the user can directly interact with their particle devices
                 launchController();
@@ -80,6 +108,111 @@ public class DashboardActivity extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         logOut();
+    }
+
+    public void createNewWidget () {
+        final ImageButton imageButton = new ImageButton(this);
+
+        // Sets the image resource to be used for our ImageView
+        imageButton.setImageResource(R.drawable.ic_settings_black_48dp);
+
+        // Set our on-click function
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            final String function = "led";
+            final List<String> commands = Arrays.asList("1");
+
+            public void onClick(View v) {
+                Async.executeAsync(ParticleCloudSDK.getCloud(), new Async.ApiWork<ParticleCloud, Integer>() {
+                    @Override
+                    public Integer callApi(ParticleCloud particleCloud) throws ParticleCloudException, IOException {
+                        try {
+                            ParticleDevice particleDevice = ParticleCloudSDK.getCloud().getDevices().get(1);
+                            return particleDevice.callFunction(function, commands);
+                        } catch (ParticleDevice.FunctionDoesNotExistException e) {
+                            e.printStackTrace();
+                        }
+                        return 1;
+                    }
+
+                    @Override
+                    public void onSuccess (Integer i) {
+                    }
+
+                    @Override
+                    public void onFailure(ParticleCloudException exception) {
+                    }
+                });
+            }
+        });
+
+        // Add our widget to our view
+        _widgetList.add(imageButton);
+        _dashboardLayout.addView(imageButton, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+    }
+
+    private void toggleEditMode () {
+        // Disable touch listener on each widget and set drag and drop listener
+        for (ImageButton imageButton: _widgetList) {
+            imageButton.setOnClickListener(null);
+
+            imageButton.setOnTouchListener(new View.OnTouchListener() {
+                float deltaX;
+                float deltaY;
+
+                @Override
+                public boolean onTouch(View view, MotionEvent event) {
+                    switch (event.getAction()) {
+                        case MotionEvent.ACTION_DOWN:
+                            deltaX = view.getX() - event.getRawX();
+                            deltaY = view.getY() - event.getRawY();
+                            break;
+                        case MotionEvent.ACTION_MOVE:
+                            view.animate()
+                                    .x(event.getRawX() + deltaX)
+                                    .y(event.getRawY() + deltaY)
+                                    .setDuration(0)
+                                    .start();
+                            break;
+                    }
+                    return true;
+                }
+            });
+        }
+    }
+
+    private void disableEditMode () {
+        // Disable drag and drop and re-enable click listener
+        for (ImageButton imageButton: _widgetList) {
+            imageButton.setOnTouchListener(null);
+
+            imageButton.setOnClickListener(new View.OnClickListener() {
+                final String function = "led";
+                final List<String> commands = Arrays.asList("1");
+
+                public void onClick(View v) {
+                    Async.executeAsync(ParticleCloudSDK.getCloud(), new Async.ApiWork<ParticleCloud, Integer>() {
+                        @Override
+                        public Integer callApi(ParticleCloud particleCloud) throws ParticleCloudException, IOException {
+                            try {
+                                ParticleDevice particleDevice = ParticleCloudSDK.getCloud().getDevices().get(1);
+                                return particleDevice.callFunction(function, commands);
+                            } catch (ParticleDevice.FunctionDoesNotExistException e) {
+                                e.printStackTrace();
+                            }
+                            return 1;
+                        }
+
+                        @Override
+                        public void onSuccess (Integer i) {
+                        }
+
+                        @Override
+                        public void onFailure(ParticleCloudException exception) {
+                        }
+                    });
+                }
+            });
+        }
     }
 
     private void launchController () {
